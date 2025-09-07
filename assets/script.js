909a86ebb02f353873987fdb2b4a39684ec2cf81
Ärthur Berlofa Bosi <berlofabosi.arthur@gmail.com>
2025-09-07 19:34:12 -0300

    fix: JS, HTML and some CSS changes

--- assets/script.js
+++ assets/script.js
@@ -7,7 +7,7 @@ let cards = [
     "gols": 15,
     "assistencias": 10,
     "jogos": 28,
-    "favorita": false
+    "favorita": true
   },
   {
     "nome": "Dayana Rodríguez",
@@ -52,49 +52,135 @@ let cards = [
 ]
 
 // Inicialização
-window.onload = function() {
-    loadCards();
-    displayCards();
+window.onload = function () {
+  loadCards();
+  saveCards();
+  displayCards();
 
-    document.getElementById('cardForm').addEventListener('submit', addCard); 
-    document.getElementById('postList').addEventListener('click', handlePostListClick);
+  document.getElementById('cardForm').addEventListener('submit', addCard);
+  document.getElementById('cardList').addEventListener('click', handleCardListClick);
 };
 
 // Função para salvar no LocalStorage
 function saveCards() {
-    localStorage.setItem("posts", JSON.stringify(cards));
+  localStorage.setItem("cards", JSON.stringify(cards));
 }
 // Função para carregar os posts do LocalStorage
 function loadCards() {
-    const storedCards = localStorage.getItem("cards");
-    if (storedCards) {
-        cards = JSON.parse(storedCards);
-    }
+  const storedCards = localStorage.getItem("cards");
+  if (storedCards) {
+    cards = JSON.parse(storedCards);
+  }
 }
 
-// Função para exibir os cards
+// Exibição dos Cards
 function displayCards() {
-    const cardList = document.getElementById('cardList');
-    cardList.innerHTML = '';
+  const cardList = document.getElementById('cardList');
+  cardList.innerHTML = '';
 
-    cards.forEach((card, index) => {
-        const cardElement = document.createElement('div');
-        cardElement.classList.add('card');
+  cards.forEach((card, index) => {
+    const cardElement = document.createElement('div');
+    cardElement.classList.add('card');
 
-        cardElement.innerHTML = `
+    cardElement.innerHTML = `
+            <div class="card-buttons">
+              <button data-action="delete" data-index="${index}"><i class="fa-solid fa-eraser"></i> Apagar</button>
+              <button data-action="edit" data-index="${index}"><i class="fa-solid fa-pen-to-square"></i> Editar</button>
+              <button data-action="toggleFavorite" data-index="${index}">
+                  ${card.favorita ? '<i class="fa-solid fa-star"></i>' : '<i class="fa-regular fa-star"></i>'}
+              </button>
+            </div>
+            <h1>${card.nome}</h1>
             <img src="${card.foto}" alt="Foto de ${card.nome}">
-            <h3>${card.nome}</h3>
             <p><strong>Posição:</strong> ${card.posicao}</p>
             <p><strong>Clube:</strong> ${card.clube}</p>
             <p><strong>Gols:</strong> ${card.gols}</p>
             <p><strong>Assistências:</strong> ${card.assistencias}</p>
             <p><strong>Jogos:</strong> ${card.jogos}</p>
-            <button data-action="toggleFavorite" data-index="${index}">
-                ${card.favorita ? '<i class="fa-solid fa-star"></i> Favorita' : '<i class="fa-regular fa-star"></i> Favorita'}
-            </button>
             <hr style="margin:30px;">
         `;
 
-        cardList.appendChild(cardElement);
-    });
+    cardList.appendChild(cardElement);
+  });
+}
+
+// Criar um novo Card
+function addCard(event) {
+  console.log(event);
+  // Impede o envio padrão do formulário
+  event.preventDefault();
+
+  // Pega todos os valores do formulário
+  const cardName = document.getElementById('cardName').value;
+  const cardPosition = document.getElementById('cardPosition').value;
+  const cardClub = document.getElementById('cardClub').value;
+  const cardStatistcsGols = document.getElementById('cardStatistcsGols').value;
+  const cardStatistcsAssist = document.getElementById('cardStatistcsAssist').value;
+  const cardStatistcsGames = document.getElementById('cardStatistcsGames').value;
+  const cardPhoto = document.getElementById('cardPhoto').value;
+
+  // Novo objeto card
+  const card = {
+    nome: cardName,
+    posicao: cardPosition,
+    clube: cardClub,
+    foto: cardPhoto,
+    gols: cardStatistcsGols,
+    assistencias: cardStatistcsAssist,
+    jogos: cardStatistcsGames,
+    favorita: false
+  };
+
+  cards.unshift(card); // Adiciona o objeto no início da array
+  saveCards(); // salva no localStorage
+
+  document.getElementById('cardForm').reset(); // Reseta o formulário
+  displayCards(); // Atualiza a exibição dos cards
+  alert("Card adicionado com sucesso!");
+}
+
+// Função para lidar com cliques na lista de cards
+function handleCardListClick(event) {
+    const clickedElement = event.target.closest("button"); // garante que pega o botão
+    if (!clickedElement) return;
+
+    const action = clickedElement.dataset.action;
+    const index = clickedElement.dataset.index;
+
+    if (action === "edit") {
+        // editCard(index);
+    } else if (action === "delete") {
+        deleteCard(index);
+    } else if (action === "toggleFavorite") {
+        cardFavorite(index);
+    }
+}
+
+// Atualiza um Card existente
+function editCard(index) {
+    const novoNome = prompt("Editar nome:", cards[index].text);
+
+    if (novoTexto !== null) {
+        posts[index].text = novoTexto;
+        savePosts();
+        displayPosts();
+    }
+    alert("Card editado com sucesso!");
+}
+// Deleta um Card existente
+function deleteCard(index) {
+    const confirma = confirm("Deseja realmente excluir esse card?");
+    if (confirma) { // Se o usuário clicar em "OK"
+        cards.splice(index, 1); // Remove o card do array
+        saveCards(); // Salva a atualização no LocalStorage
+        displayCards(); // Atualiza a exibição dos cards
+        alert("Card excluído com sucesso!");
+    }
+}
+
+// Atualiza se é favorito ou não
+function cardFavorite(index) {
+    cards[index].favorita = !cards[index].favorita; // Alterna o status de favorito
+    saveCards(); // Salva a atualização no LocalStorage
+    displayCards(); // Atualiza a exibição dos cards
 }
